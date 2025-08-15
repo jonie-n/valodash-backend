0e06aea0265f668860d9f6452e46877f18fb1fff
jonie-n <jonie_nishimura@brown.edu>
2025-08-15 11:52:56 -0700

    fixed cors

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://https://valodash.vercel.app/"
+    "https://valodash.vercel.app/"
   ],
   credentials: true
 }));
